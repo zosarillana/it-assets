1f2c0220bc4cfb6dd19e7b5cf2fbd3b71cb749a5
zosarillana <zhou991818@gmail.com>
2024-10-14 16:59:39 +0800

    Initial commit

---
+++ asset-export/asset-export.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-export',
+  templateUrl: './asset-export.component.html',
+  styleUrls: ['./asset-export.component.scss']
+})
+export class AssetExportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ asset-import/asset-import.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-import',
+  templateUrl: './asset-import.component.html',
+  styleUrls: ['./asset-import.component.scss']
+})
+export class AssetImportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
